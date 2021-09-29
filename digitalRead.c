d00caf4e63ebd196d90e82b7fa12a7729efa4c78
Agamemnnon <agamemnonaskyriazis@gmail.com>
2021-09-29 13:16:35 +0300

    Update digitalRead.c

--- digitalRead.c
+++ digitalRead.c
@@ -2,12 +2,14 @@
 
 int main()
 {
+	signal(SIGINT, handle_interrupt); //interrupt handler for Ctrl-C
+	
 	export_pin("24"); //Open gpio 24.
 
-	pin_mode("gpio24", "in"); //Declare gpio 24 as input.
+	set_pin_mode("24", "in"); //Declare gpio 24 as input.
 	//Loop for some time...
 	for(int i = 0; i < 1000; i++){
-		printf("gpio24 = %d\n", digital_read("gpio24")); //Read gpio24 and print its value.
+		printf("gpio24 = %d\n", read_pin_state("24")); //Read gpio 24 and print its value.
 		usleep(10*1000); //Sleep for 10ms.
 	}
 
